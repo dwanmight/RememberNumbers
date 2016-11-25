af887df24bb34ab8e38653d39ee5a81be7e2109a
dwanmight <dwanmight@gmail.com>
2016-11-26 00:06:23 +0200

    CreatedNumberUtils

--- app/src/main/java/com/junior/dwan/remembernumbers/utils/NumberUtils.java
+++ app/src/main/java/com/junior/dwan/remembernumbers/utils/NumberUtils.java
@@ -1,8 +1,21 @@
 package com.junior.dwan.remembernumbers.utils;
 
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by Might on 25.11.2016.
  */
 
 public class NumberUtils {
+
+    public static void streamNumbersPanel(TextView tvAnswer,View v) {
+        TextView textView = (TextView) v;
+        String working = tvAnswer.getText().toString();
+        String text = textView.getText().toString();
+        if (working.equals("0")) {
+            tvAnswer.setText(text);
+        } else
+            tvAnswer.setText(working + text);
+    }
 }
